bda8772ff25dd894b3f442d79833fe92c0f2a9c1
taejaa <ksw210609@gmail.com>
2022-09-21 17:08:51 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
 def print_hello():
     print("Hello World!!")
 
+def printf_KIT():
+    printf("Kumoh National Institute of Technology")
+Institute of Technology
+
 if __name__ == '__main__':
+    printf_KIT()
     print_hello()
